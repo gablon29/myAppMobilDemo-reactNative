a20be460f57c6d0b6aba2930ad12e564e635e0fc
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-04 18:56:25 -0300

    included style on the project

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Button, Alert, View, Text, Image, ScrollView, TextInput, TouchableWithoutFeedback} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>I love you so much my beautifull wife</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text style={{ 
+        justifyContent: 'center',
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'magenta',}}>Florencia la mas hermosa</Text>
+      <View>
+        <Text style={{
+          textAlign: 'center',
+          fontSize: 20,
+          fontWeight: 'bold',
+          color: 'magenta'
+        }}>La mejor kinesiologa</Text>
+        </View>
+        <View>
+        <TouchableWithoutFeedback onPress={() => Alert.alert('MIAU!!')}>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{
+            width: 'auto', height: 400, margin: 5,}}
+        />
+      </TouchableWithoutFeedback>
+      </View>
+      <Button title='holis' onPress={() => Alert.alert('Buenas noches')}></Button>
+      <TextInput
+        style={{
+          justifyContent:'center',
+          textAlign: 'center',
+          fontSize: 18,
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="I miss you so much my baby!"
+      />
+    </ScrollView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
