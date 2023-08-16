aa5aecf7a2a37199ea5c0c042383484edd8c4134
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-16 20:46:58 -0300

    mostrando los primeros elementos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Constants from 'expo-constants'
 import { View, Text } from "react-native";
+import RepositoriesList from "./RepositoriesList";
 
 const Main = () => {
     return (
         <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
             <Text>Rate Repository Application</Text>
+            <RepositoriesList />
         </View>
     )
 }
---
+++ src/components/RepositoriesList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { repositories } from "../data/repositories";
+
+const RepositoriesList = () => {
+    return (
+        <View>
+            {
+                repositories.map(repo => (
+                    <View key={repo.id}>
+                        <Text>{repo.fullname}</Text>
+                    </View>
+                ))
+            }
+        </View>
+    )
+}
+
+export default RepositoriesList
