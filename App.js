75943163e8b7188c350133eafef43767a6225c48
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-16 19:12:52 -0300

    incluyendo readme para la guia del proyecto

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Button, Alert, View, Text, Image, ScrollView, TextInput, TouchableWithoutFeedback} from 'react-native';
+import { Button, Alert, View, Text, Image, ScrollView, TextInput, TouchableWithoutFeedback } from 'react-native';
 
 const App = () => {
   return (
     <ScrollView>
-      <Text style={{ 
+      <Text style={{
         justifyContent: 'center',
         textAlign: 'center',
         fontSize: 20,
         fontWeight: 'bold',
-        color: 'magenta',}}>Florencia la mas hermosa</Text>
+        color: 'magenta',
+      }}>Florencia la mas hermosa</Text>
       <View>
         <Text style={{
           textAlign: 'center',
@@ -17,22 +18,23 @@ const App = () => {
           fontWeight: 'bold',
           color: 'magenta'
         }}>La mejor kinesiologa</Text>
-        </View>
-        <View>
+      </View>
+      <View>
         <TouchableWithoutFeedback onPress={() => Alert.alert('MIAU!!')}>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{
-            width: 'auto', height: 400, margin: 5,}}
-        />
-      </TouchableWithoutFeedback>
+          <Image
+            source={{
+              uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+            }}
+            style={{
+              width: 'auto', height: 400, margin: 5,
+            }}
+          />
+        </TouchableWithoutFeedback>
       </View>
       <Button title='holis' onPress={() => Alert.alert('Buenas noches')}></Button>
       <TextInput
         style={{
-          justifyContent:'center',
+          justifyContent: 'center',
           textAlign: 'center',
           fontSize: 18,
           height: 40,
---
+++ Readme.md
@@ -0,0 +1,2 @@
+<h1>Bienvenidos a mi aplicacion demo de react-native</h1>
+<p>Para inicializar el proyecto primero debemos ejecutar el comando <bold>npx expo</bold></p>
