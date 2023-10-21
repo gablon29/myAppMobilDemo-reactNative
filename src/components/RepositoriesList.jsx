024c4c19ad468a63f1d9ad7e8a77fdd45f115441
gablon29 <gabriellondero40@gmail.com>
2023-10-21 19:42:19 -0300

    creando la api para la coleccion de datos y desarrollando la peticion

--- src/components/RepositoriesList.jsx
+++ src/components/RepositoriesList.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, Text } from "react-native";
-import { repositories } from "../data/repositories";
 import RepositoriesItem from "./RepositoriesItem";
 
 const RepositoriesList = () => {
+  const [repositories, setRepositories] = useState(null);
+  const repositoriesResp = () => {
+    return fetch("http://192.168.100.4:5000/api/repositories")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    repositoriesResp();
+  }, []);
+
+  const repositoriesNode = repositories
+    ? repositories.edges.map((edges) => edges.node)
+    : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoriesNode}
       renderItem={({ item: repo }) => <RepositoriesItem {...repo} />}
     />
   );
