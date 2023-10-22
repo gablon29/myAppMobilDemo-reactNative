6c473745d7ce7f5ce339e23efaa2b3f8a5bca967
gablon29 <gabriellondero40@gmail.com>
2023-10-22 18:01:35 -0300

    agragando las peticiones con apollo

--- src/components/RepositoriesList.jsx
+++ src/components/RepositoriesList.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList } from "react-native";
 import RepositoriesItem from "./RepositoriesItem";
 import useRepositories from "../hooks/useRepositories";
 
 const RepositoriesList = () => {
   const { repositories } = useRepositories();
+
   return (
     <FlatList
       data={repositories}
